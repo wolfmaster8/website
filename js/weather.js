a0d1197631c2b70892b6a064ac3a513b278159cd
Felipe Lobo <jflobom@gmail.com>
2019-11-03 23:27:14 -0300

    lint fixes

--- js/weather.js
+++ js/weather.js
@@ -1,8 +1,7 @@
-const getMainInformation = (info) => {
+
+function getMainInformation(info) {
     return {
-        location: {
-            city: info.name,
-        },
+        location: {city: info.name},
         atmospheric: {
             temperature: convertTemperatureToCelsius(info.main.temp),
             pressure: info.main.pressure,
@@ -10,50 +9,49 @@ const getMainInformation = (info) => {
         },
         weather: {
             main: info.weather[0].main,
-            description: info.weather[0].description,
+            description: info.weather[0].description
         },
         icon: getIcon(info.weather[0].main.toLowerCase()),
         theme: defineTheme()
     }
-}
-
-const convertTemperatureToCelsius = (kelvin) => {
-    return (kelvin - 273.15).toFixed(0)
 };
 
-const defineTheme = () => {
+function convertTemperatureToCelsius(kelvin) {
+    return (kelvin - 273.15).toFixed(0);
+};
+
+function defineTheme() {
     const date = new Date();
     const hours = date.getHours();
-    console.log(hours);
     if (hours <= 5 || hours >= 18) {
-        return 'dark'
+        return 'dark';
     } else {
-        return 'light'
+        return 'light';
     }
 };
 
-const getIcon = (type) => {
+function getIcon(type){
     switch (type) {
         case 'clouds':
-            return {icon: 'cloud', color: 'gray'}
+            return {icon: 'cloud', color: 'gray'};
         case 'thunderstorm':
-            return {icon: 'cloud-showers-heavy', color: 'rain'}
+            return {icon: 'cloud-showers-heavy', color: 'rain'};
         case 'rain':
             return {icon: 'cloud-rain', color: 'rain'};
         case 'drizzle':
-            return {icon: 'cloud-sun-rain', color: 'gray'}
+            return {icon: 'cloud-sun-rain', color: 'gray'};
         case 'clear':
-            return {icon: 'sun', color: 'yellow'}
+            return {icon: 'sun', color: 'yellow'};
         case 'snow':
             return {icon: 'snowflake', color: 'rain'};
         default:
-            return {icon: 'cloud', color: 'gray'}
+            return {icon: 'cloud', color: 'gray'};
     }
 };
 
 async function getWeather(city = 'Bogota') {
     const weather = await $.ajax({
-        url: `https://api.openweathermap.org/data/2.5/weather?APPID=9f1cb634ec747082eb0732356b6d147b&q=${city},co`
+        url: `https://api.openweathermap.org/data/2.5/weather?APPID=9f1cb634ec747082eb0732356b6d147b&q=${city}`
     });
-    return getMainInformation(weather)
+    return getMainInformation(weather);
 }
